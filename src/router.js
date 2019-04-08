6239c4d3ad54ea0063c2fb3bbd5181e2ad67682a
z.xin <z.xin@WKS-XIN>
2019-04-08 15:15:46 +0200

    adds User.vue

--- src/router.js
+++ src/router.js
@@ -20,7 +20,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }, {
-      path: '/*',
+      path: '/user/:id',
+      component: (resolve) => require(['./views/User.vue'], resolve)
+    }, {
+      path: '*',
       redirect: '/home'
     }
   ]
---
+++ src/views/User.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    UserPage
+    UserId: {{this.$route.params.id}}
+</div>
+</template>
+<script>
+export default {
+  mounted () {
+    console.log(this.$route.params.id)
+  }
+}
+</script>
